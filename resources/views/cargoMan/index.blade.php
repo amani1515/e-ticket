af09e5f193fbb7f1ae504418de036dda3f9de24d
Amanuel <amanuelmhret16@gmail.com>
2025-05-24 16:59:26 +0300

    interfere

--- resources/views/cargoMan/index.blade.php
+++ resources/views/cargoMan/index.blade.php
@@ -1,14 +1,5 @@
-{{-- filepath: resources/views/cargoMan/index.blade.php --}}
-@extends('cargoMan.layout.app')
-
-@section('title', 'Cargo Man Home')
-
-@section('content')
-    <h1 class="text-2xl font-bold mb-4">Cargo Man Dashboard</h1>
-    {{-- Add dashboard content here --}}
-
-    <form method="POST" action="{{ route('logout') }}" class="mt-6">
-        @csrf
-        <button class="text-red-700 hover:underline font-semibold">Logout</button>
-    </form>
-@endsection
+<h1>Cargo Man</h1>
+<form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button class="text-red-700 hover:underline">Logout</button>
+        </form>
