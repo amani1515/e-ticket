13bf8de76b2e9f02c53cdbda938fa17150bd0506
Amanuel <amanuelmhret16@gmail.com>
2025-05-24 16:57:19 +0300

    cargo page

--- resources/views/cargoMan/index.blade.php
+++ resources/views/cargoMan/index.blade.php
@@ -6,4 +6,9 @@
 @section('content')
     <h1 class="text-2xl font-bold mb-4">Cargo Man Dashboard</h1>
     {{-- Add dashboard content here --}}
+
+    <form method="POST" action="{{ route('logout') }}" class="mt-6">
+        @csrf
+        <button class="text-red-700 hover:underline font-semibold">Logout</button>
+    </form>
 @endsection
