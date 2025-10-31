f02203bc0047ac04119482235c29b9993bd523a5
Amanuel <amanuelmhret16@gmail.com>
2025-10-31 21:47:15 +0300

    button included on receipt to back and refresh

--- resources/views/ticketer/tickets/receipt.blade.php
+++ resources/views/ticketer/tickets/receipt.blade.php
@@ -284,7 +284,7 @@
     function printReceipt() {
         window.print();
         
-        // Refresh page after print dialog closes to show REPRINTED text
+        // Refresh page after print dialog closes to show REPRINTED teext
         setTimeout(function() {
             window.location.reload();
         }, 1000);
